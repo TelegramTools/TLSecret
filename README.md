b6f92db5b24ab8a1f08e79d4a05e8c4930d1061d
ferferga <ferferga.fer@gmail.com>
2018-07-09 11:58:41 +0200

    TLSecret 1.0 release

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
+<p align="center">
+  <img src="https://github.com/TelegramTools/TLSecret/raw/master/images/Intro.png">
+ </p>
+
 # TLSecret
-An small script for simplifying the authentication of other Telegram's users in other Telegram Tools apps
+An small script for simplifying the authentication of other Telegram's users in all Telegram Tools apps
+
+Some Telegram Tools apps requires you to sign in two users in Telegram. With TLSecret, you can authenticate and pass your Telegram's keys using the "Secret Mode" on all Telegram Tools apps, without the need of giving your phone number, code and password to your partner's using one of the other Telegram Tools apps.
+This way, you have full control of your account whenever you want or need to use apps like TLImporter with another person.
+
+# How to use it?
+
+**This instructions assumes that the person who is reading this is the one who wants to grant access into a Telegram account to a person using any of the Telegram Tools apps.**
+
+When using the "Secret Mode", there are two apps involved: 
+· The **listener** app (TLRevert, TLMerger, TLImporter and any other Telegram Tools' app that can use the "Secret Mode" to login users). This will be the app where you want to be logged in remotely. It can be running in your partner's computer
+· TLSecret, which will be running in your own computer.
+
+As you are the one who wants to grant access to your account, you will be using TLSecret app.
+
+## Preparation
+
+The person using the **listener** app must have entered into the "Secret Mode" (every Telegram Tools app which supports "Secret Mode" will prompt the user at some point whether using "Secret Mode" or not).
+After entering into the "Secret Mode", the chat list of your partner will be displayed. He must choose you (the person using TLSecret). After that, a message saying "Waiting for a response..." will be displayed in his chat.
+
+## Using TLSecret
+
+Once the **listener** app is in "Waiting for a response..." mode, you can use TLSecret.
+
+Input your phone number, verification code and password, just like you would do in other Telegram app. Your chat list will be displayed after the authentication is done succesfully. Choose your partner there (the person using the **listener** app).
+
+After that, TLSecret will work behind-the-scenes, and will authorize yourself in your partner's computer. You are done!
+
+## Final tips
+
+After the authentication is done succesfully in the **listener** app, you will be always in control of your session. By pressing ENTER, you will revoke the access to your account and log out your friend of Telegram.
+If you have closed the app after the authorization is done, don't worry!. Next time you run it, you will be able to revoke the session by pressing ENTER as well.
+
+# Credits
+
+Secret mode couldn't be possible without [Telethon](https://github.com/LonamiWebs/Telethon), and his great creator, [Lonami](https://github.com/Lonami), who always was up to answering questions and helping in development. I'm so grateful for his patience :).
+Also, huge thanks to [pyAesCrypt](https://github.com/marcobellaccini/pyAesCrypt)
+Huge thanks to all the team behind [PyInstaller](https://www.pyinstaller.org/), which I used to build the Windows binaries.
+
+**Give always credits to all the original authors and owners when using some parts of their hard work in your own projects**
+
---
+++ src/TLSecret.py
@@ -0,0 +1,145 @@
+import datetime
+import getpass
+import logging
+import shutil
+import sqlite3
+import sys
+import time
+import cryptg
+from cryptg import *
+from os import mkdir
+
+import progressbar  # progressbar2 module
+from telethon import TelegramClient, events
+from telethon.events import StopPropagation
+from telethon.errors import FloodWaitError
+from telethon.tl.functions.messages import *
+from telethon.tl.types import *
+from telethon.utils import *
+import pyAesCrypt
+import logging
+
+password = ##THIS IS THE PASSWORD THAT WILL BE USED TO ENCRYPT DATABASES USING TLSecret. YOU MUST USE THE SAME PASSWORD IN THE RECEIVING APP AND TLSECRET.
+bufferSize = 64 * 1024
+api_id = ##INSERT YOUR APIID HERE
+api_hash = ##INSERT YOUR APIHASH HERE
+TLdevice_model = 'Desktop device'
+TLsystem_version = 'Console'
+TLapp_version = '- TLSecret 1.0'
+TLlang_code = 'en'
+TLsystem_lang_code = 'en'
+dialogs = None
+ChosenChat = None
+file = None
+
+client1 = TelegramClient("User2", api_id, api_hash, device_model=TLdevice_model, system_version=TLsystem_version, app_version=TLapp_version, lang_code=TLlang_code, system_lang_code=TLsystem_lang_code)
+logging.basicConfig(filename="TLSecretHelper-log.log", level=logging.DEBUG, format='%(asctime)s %(message)s')
+
+def StartClient1():
+    global client1
+    try:
+        client1.connect()
+        if not client1.is_user_authorized():
+            client1.start(force_sms=False)
+        SelfUser1 = client1.get_me()
+    except:
+        if not client1.is_connected():
+            getpass.getpass("You are not connected to the internet or the phone was given in the incorrect format. Check your connection and press ENTER to try again: ")
+        StartClient1()
+    return
+
+@client1.on(events.NewMessage(chats=ChosenChat, incoming=True))
+def EventHandler(event):
+    if "WooHoo!" in event.raw_text:
+        print("\nResponse received!")
+        client1.delete_messages(ChosenChat, [file.id, event.original_message.id], revoke=True)
+        client1.disconnect()
+    return
+
+def sprint(string, *args, **kwargs):
+    #Safe Print (handle UnicodeEncodeErrors on some terminals)
+    try:
+        print(string, *args, **kwargs)
+    except UnicodeEncodeError:
+        string = string.encode('utf-8', errors='ignore')\
+                       .decode('ascii', errors='ignore')
+        print(string, *args, **kwargs)
+
+def PrintChatList():
+    global dialogs
+    while True:
+        dialogs = client1.get_dialogs(limit=None)
+        i = None
+        while i is None:
+            print("This is the chat list. Choose your partner:\n\n")
+            for i, dialog in enumerate(dialogs, start=1):
+                if get_display_name(dialog.entity) is "":
+                    name = "Deleted Account"
+                elif isinstance(dialog.entity, InputPeerSelf):
+                    name = "Chat with yourself (Saved Messages)"
+                else:
+                    name = get_display_name(dialog.entity)
+                sprint('{}. {}'.format(i, name))
+
+            print()
+            print('> Who is your partner?')
+            print('> Available commands:')
+            print('  !q: Quits the dialogs window and exits.')
+            print('  !l: Logs out, terminating this session.')
+            print()
+            i = input('Enter dialog ID or a command to continue: ')
+            if i is None:
+                continue
+            if i == '!q':
+                sys.exit()
+            if i == '!l':
+                client1.log_out()
+                sys.exit()
+        try:
+            i = int(i if i else 0) - 1
+            # Ensure it is inside the bounds, otherwise retry
+            dialog_count = dialogs.total
+            if not 0 <= i < dialog_count:
+                i = None
+        except:
+            i = None
+            print("That's not a valid Chat. Please, try again.")
+            continue
+
+            # Retrieve the selected user (or chat, or channel)
+        return dialogs[i].entity
+
+##ENTRY POINT OF THE CODE
+client1.connect()
+print("\nWelcome! This app will create an encrypted session file and will send it through Telegram to your partner for using it with other Telegram's Tools apps\n\nWARNING: Although this makes decrypting the session file or accessing your account difficult for your partner, there are some ways to get the password and decrypt it using reverse-engineering means. Use it with a trustful person and with this consideration in mine.\nYou will always have the option to revoke the session file for your partner if you see that something isn't going as expected.")
+getpass.getpass("\n\n\nPress ENTER to continue")
+
+if not client1.is_user_authorized():
+    print("\nNow, it's time to login in Telegram\n")
+    StartClient1()
+    print("Done! Connected to Telegram!")
+    client1.disconnect()
+    pyAesCrypt.encryptFile("User2.session", "DB.aes", password, bufferSize)
+    StartClient1()
+    print("Gathering your chat list...")
+    ChosenChat = PrintChatList()
+    dialogs.clear()
+    print("\n\nSending your session file to your partner...")
+    file = client1.send_file(ChosenChat, "DB.aes")
+    print("\n\nWaiting for a response...")
+    client1.run_until_disconnected()
+    print("Your partner has been authorised in Telegram successfully!\nYou can keep this window open for revoking the session in case it's necessary.\nYou can close this app if you don't want to revoke it.\n\n")
+    getpass.getpass("\n\nPress ENTER to revoke the session: ")
+    print("\nLogging out...")
+    client1.connect()
+    client1.log_out()
+    print("\n\nLogged out!")
+else:
+    getpass.getpass("There is already a session logged in. Press ENTER to revoke it: ")
+    try:
+        os.remove("DB.aes")
+    except:
+        pass
+    client1.log_out()
+    print("Done!")
+getpass.getpass("Press ENTER to close TLSecret...")

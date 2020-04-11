bd47d01af171a39ca7177d19d801aae2bdf30024
ferferga <ferferga.fer@gmail.com>
2020-04-11 18:45:13 +0200

    Update secret mode to be compatible with new apps and import getpass only

--- src/TLSecret.py
+++ src/TLSecret.py
@@ -1,34 +1,25 @@
-##### THIS SCRIPT HAS BEEN MADE BY FERFERGA. PLEASE, DON'T CLAIM THAT IT'S YOURS.
-##### GIVE ALWAYS CREDITS TO ORIGINAL AUTHORS.
-#####
-##### THANKS FOR USING!
-
-import datetime
-import getpass
-import logging
-import shutil
-import sqlite3
-import sys
-import time
+#!/usr/bin/python3
+import getpass, logging, sys, time, io
 try:
     import cryptg
     from cryptg import *
 except:
     pass
-from os import mkdir
-
-import progressbar  # progressbar2 module
-from telethon import TelegramClient, events
+import os
+from getpass import getpass
+from sys import exit
+from telethon.sync import TelegramClient, events
 from telethon.events import StopPropagation
 from telethon.errors import FloodWaitError
 from telethon.tl.functions.messages import *
 from telethon.tl.types import *
 from telethon.utils import *
+from telethon.sessions import StringSession
 import pyAesCrypt
 import logging
 
 password = "PASSWORD_FOR_SECRET_MODE_HERE"
-bufferSize = 64 * 1024
+bufferSize = 64 * 4096
 api_id = YOUR_API_ID_HERE
 api_hash = 'YOUR_API_HASH_HERE'
 TLdevice_model = 'Desktop device'
@@ -40,8 +31,7 @@ dialogs = None
 ChosenChat = None
 file = None
 
-client1 = TelegramClient("User2", api_id, api_hash, device_model=TLdevice_model, system_version=TLsystem_version, app_version=TLapp_version, lang_code=TLlang_code, system_lang_code=TLsystem_lang_code, spawn_read_thread=False, update_workers=1)
-#TELETHON 1.0 AND FORWARD: client1 = TelegramClient("User2", api_id, api_hash, device_model=TLdevice_model, system_version=TLsystem_version, app_version=TLapp_version, lang_code=TLlang_code, system_lang_code=TLsystem_lang_code)
+client1 = TelegramClient("User2", api_id, api_hash, device_model=TLdevice_model, system_version=TLsystem_version, app_version=TLapp_version, lang_code=TLlang_code, system_lang_code=TLsystem_lang_code)
 logging.basicConfig(filename="TLSecretHelper-log.log", level=logging.DEBUG, format='%(asctime)s %(message)s')
 
 def StartClient1():
@@ -53,16 +43,16 @@ def StartClient1():
         SelfUser1 = client1.get_me()
     except:
         if not client1.is_connected():
-            getpass.getpass("You are not connected to the internet or the phone was given in the incorrect format. Check your connection and press ENTER to try again: ")
+            getpass("You are not connected to the internet or the phone was given in the incorrect format. Check your connection and press ENTER to try again: ")
         StartClient1()
     return
 
 @client1.on(events.NewMessage(chats=ChosenChat, incoming=True))
-def EventHandler(event):
+async def EventHandler(event):
     if "WooHoo!" in event.raw_text:
         print("\nResponse received!")
-        client1.delete_messages(ChosenChat, [file.id, event.original_message.id], revoke=True)
-        client1.disconnect()
+        await client1.delete_messages(ChosenChat, [file.id, event.message.id], revoke=True)
+        await client1.disconnect()
     return
 
 def sprint(string, *args, **kwargs):
@@ -82,7 +72,7 @@ def PrintChatList():
         while i is None:
             print("This is the chat list. Choose your partner:\n\n")
             for i, dialog in enumerate(dialogs, start=1):
-                if get_display_name(dialog.entity) is "":
+                if get_display_name(dialog.entity) == "":
                     name = "Deleted Account"
                 elif isinstance(dialog.entity, InputPeerSelf):
                     name = "Chat with yourself (Saved Messages)"
@@ -100,10 +90,10 @@ def PrintChatList():
             if i is None:
                 continue
             if i == '!q':
-                sys.exit()
+                exit(1)
             if i == '!l':
                 client1.log_out()
-                sys.exit()
+                exit(0)
         try:
             i = int(i if i else 0) - 1
             # Ensure it is inside the bounds, otherwise retry
@@ -121,14 +111,17 @@ def PrintChatList():
 ##ENTRY POINT OF THE CODE
 client1.connect()
 print("\nWelcome! This app will create an encrypted session file and will send it through Telegram to your partner for using it with other Telegram's Tools apps\n\nWARNING: Although this makes decrypting the session file or accessing your account difficult for your partner, there are some ways to get the password and decrypt it using reverse-engineering means. Use it with a trustful person and with this consideration in mine.\nYou will always have the option to revoke the session file for your partner if you see that something isn't going as expected.")
-getpass.getpass("\n\n\nPress ENTER to continue")
+getpass("\n\n\nPress ENTER to continue")
 
 if not client1.is_user_authorized():
     print("\nNow, it's time to login in Telegram\n")
     StartClient1()
     print("Done! Connected to Telegram!")
     client1.disconnect()
-    pyAesCrypt.encryptFile("User2.session", "DB.aes", password, bufferSize)
+    print(StringSession.save(client1.session))
+    sess = io.BytesIO(StringSession.save(client1.session).encode())
+    with open("DB.aes", "wb") as fOut:
+        pyAesCrypt.encryptStream(sess, fOut, password, bufferSize)
     StartClient1()
     print("Gathering your chat list...")
     ChosenChat = PrintChatList()
@@ -136,10 +129,9 @@ if not client1.is_user_authorized():
     print("\n\nSending your session file to your partner...")
     file = client1.send_file(ChosenChat, "DB.aes")
     print("\n\nWaiting for a response...")
-    #client1.run_until_disconnected() UNCOMMENT FOR TELETHON VERSION 1.0 AND FORWARD
-    client1.idle()
-    print("Your partner has been authorised in Telegram successfully!\nYou can keep this window open for revoking the session in case it's necessary.\nYou can close this app if you don't want to revoke it.\n\n")
-    getpass.getpass("\n\nPress ENTER to revoke the session: ")
+    client1.run_until_disconnected()
+    print("Your partner has been authorised in Telegram successfully!\nYou can keep this window open for revoking the session in case you think it's necessary.\nYou can close this app if you don't want to revoke it.\n\n")
+    getpass("\n\nPress ENTER to revoke the session: ")
     print("\nLogging out...")
     while not client1.is_connected():
         client1.connect()
@@ -151,7 +143,7 @@ if not client1.is_user_authorized():
     client1.log_out()
     print("\n\nLogged out!")
 else:
-    getpass.getpass("There is already a session logged in. Press ENTER to revoke it: ")
+    getpass("There is already a session logged in. Press ENTER to revoke it: ")
     while not client1.is_connected():
         client1.connect()
         if client1.is_connected():
@@ -165,4 +157,4 @@ else:
     except:
         pass
     print("Done!")
-getpass.getpass("Press ENTER to close TLSecret...")
+getpass("Press ENTER to close TLSecret...")
